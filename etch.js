d5eefe5d3163d1df48bee0613ee11bc7970d40b3
Jaden <jenzbr@gmail.com>
2021-10-07 14:44:23 -0500

    added reset button

--- etch.js
+++ etch.js
@@ -3,6 +3,7 @@
 //var div = document.createElement('div')
 
 const container = document.getElementById('cont')
+const btn = document.getElementById('button')
 //container.style.background = "blue";
 //container.style.gridTemplateColumns = "1fr 1fr 1fr";
 
@@ -11,10 +12,13 @@ for (let i = 1; i <= 256; i++) {
   let div = document.createElement('div')
   //div.innerHTML = 'x';
   div.classList.add('gridSquare');
+  div.setAttribute('id', 'divvy')
   container.appendChild(div);
   div.addEventListener('mouseover', e => {
   div.style.background = 'red';
 });
+  btn.addEventListener('click', e => {
+  //let div = document.querySelectorAll('divvy');
+  div.style.background = 'lightgreen';
+})
 }
-
-
